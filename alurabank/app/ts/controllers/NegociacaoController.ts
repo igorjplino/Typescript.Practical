f83c73582a06f77080c55459d7b9ddaa406a79e2
Igor Jorge Pontes Lino <igorjplino@gmail.com>
2020-03-19 12:46:03 -0300

    Continuação do curso

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,29 +1,28 @@
 import { MensagemView, NegociacoesView } from '../views/index';
-import { Negociacoes, Negociacao } from '../models/index';
-import { logarTempoDeExecucao } from '../helpers/decorators/index';
+import { Negociacoes, Negociacao, NegociacaoParcial } from '../models/index';
+import { domInject, throttle } from '../helpers/decorators/index';
+
+let timer = 0;
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+    @domInject('#valor')
     private _inputValor: JQuery;
+
     private _negociacoes: Negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView("#negociacoesView");
     private _mensagemView = new MensagemView("#mensagemView");
 
     constructor() {
 
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-
         this._negociacoesView.update(this._negociacoes);
     }
 
-    @logarTempoDeExecucao()
-    adiciona(event: Event) {
-
-        event.preventDefault();
+    adiciona() {
 
         let date = new Date(this._inputData.val().replace(/-/g, ","));
 
@@ -42,6 +41,32 @@ export class NegociacaoController {
 
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update("Negociação adicionada com sucesso!");
+    }
+
+    @throttle()
+    importarDados() {
+        
+        function isOk(res: Response){
+
+            if (res.ok){
+                return res;
+            }
+            else {
+                throw new Error(res.statusText);
+            }
+        }
+
+        fetch("http://localhost:8080/dados")
+                .then(res => isOk(res))
+                .then(res => res.json())
+                .then((dados: NegociacaoParcial[]) => {
+                    dados
+                        .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                        .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+
+                    this._negociacoesView.update(this._negociacoes);
+                })
+                .catch(err => console.log(err));
     }
 
     private _ehDiaUtil(date: Date): boolean {
